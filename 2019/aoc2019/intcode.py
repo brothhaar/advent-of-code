e5172503f5e8d77c6df6d82cdd56f759fa998960
Brian Rothhaar <brian@cloud-elements.com>
2020-03-08 13:55:16 -0600

    day 5 part 2

--- 2019/aoc2019/intcode.py
+++ 2019/aoc2019/intcode.py
@@ -33,33 +33,79 @@ class IntCodeComputer:
     def op_add(self, ip, code, param_modes):
         op1 = get_operand(code, ip + 1, param_modes[0])
         op2 = get_operand(code, ip + 2, param_modes[1])
-        out_pos = code[ip + 3]
-        code[out_pos] = op1 + op2
+        op3 = code[ip + 3]
+        code[op3] = op1 + op2
+        print('%d add %d %d => %d' % (ip, op1, op2, op3))
         return ip + 4, code
 
     def op_mul(self, ip, code, param_modes):
         op1 = get_operand(code, ip + 1, param_modes[0])
         op2 = get_operand(code, ip + 2, param_modes[1])
-        out_pos = code[ip + 3]
-        code[out_pos] = op1 * op2
+        op3 = code[ip + 3]
+        code[op3] = op1 * op2
+        print('%d mul %d %d => %d' % (ip, op1, op2, op3))
         return ip + 4, code
 
     def op_input(self, ip, code, param_modes):
-        out_pos = code[ip + 1]
-        code[out_pos] = self.input_value
+        op1 = code[ip + 1]
+        code[op1] = self.input_value
+        print('%d input %d => %d' % (ip, self.input_value, op1))
         return ip + 2, code
 
     def op_output(self, ip, code, param_modes):
-        in_pos = code[ip + 1]
-        self.output_value = code[in_pos]
+        op1 = get_operand(code, ip + 1, param_modes[0])
+        self.output_value = op1
+        print('%d output %d => %d' % (ip, self.output_value, op1))
         print('OUTPUT %d' % self.output_value)
         return ip + 2, code
+
+    def op_jump_if_true(self, ip, code, param_modes):
+        op1 = get_operand(code, ip + 1, param_modes[0])
+        op2 = get_operand(code, ip + 2, param_modes[1])
+        print('%d jump_if_true %d %d' % (ip, op1, op2))
+        if op1 != 0:
+            return op2, code
+        return ip + 3, code
+
+    def op_jump_if_false(self, ip, code, param_modes):
+        op1 = get_operand(code, ip + 1, param_modes[0])
+        op2 = get_operand(code, ip + 2, param_modes[1])
+        print('%d jump_if_false %d %d' % (ip, op1, op2))
+        if op1 == 0:
+            return op2, code
+        return ip + 3, code
+
+    def op_less_than(self, ip, code, param_modes):
+        op1 = get_operand(code, ip + 1, param_modes[0])
+        op2 = get_operand(code, ip + 2, param_modes[1])
+        op3 = code[ip + 3]
+        print('%d less_than %d %d => %d' % (ip, op1, op2, op3))
+        if op1 < op2:
+            code[op3] = 1
+        else:
+            code[op3] = 0
+        return ip + 4, code
+
+    def op_equals(self, ip, code, param_modes):
+        op1 = get_operand(code, ip + 1, param_modes[0])
+        op2 = get_operand(code, ip + 2, param_modes[1])
+        op3 = code[ip + 3]
+        print('%d equals %d %d => %d' % (ip, op1, op2, op3))
+        if op1 == op2:
+            code[op3] = 1
+        else:
+            code[op3] = 0
+        return ip + 4, code
 
     operations = {
         1: op_add,
         2: op_mul,
         3: op_input,
-        4: op_output
+        4: op_output,
+        5: op_jump_if_true,
+        6: op_jump_if_false,
+        7: op_less_than,
+        8: op_equals
     }
 
     def compute(self, code):
